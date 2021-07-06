93a674bb4b57cf445927a7d77b2218432c580cbe
Gokce Ozel <norm.developer@algomedi.com>
2021-07-06 16:05:10 +0300

    Create Model and Table Storage Class

---
+++ AzureStorage.Library/ConnectionStrings.cs
@@ -0,0 +1,7 @@
+﻿namespace AzureStorage.Library
+{
+    static class ConnectionStrings
+    {
+        public static string AzureStorageConnectionString { get; set; }
+    }
+}
---
+++ AzureStorage.Library/INoSqlStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace AzureStorage.Library
+{
+    public interface INoSqlStorage<TEntity>
+    {
+        Task<TEntity> Add(TEntity entity);
+        Task<TEntity> Get(string rowKey, string partitionKey);
+        Task Delete(string rowKey, string partitionKey);
+        Task<TEntity> Update(TEntity entity);
+        IQueryable<TEntity> GetAll();
+        IQueryable<TEntity> Query(Expression<Func<TEntity,bool>> query);
+    }
+}
