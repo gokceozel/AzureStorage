bbe84994aca0b481cf80bf892912d56158cb6f92
Gokce Ozel <norm.developer@algomedi.com>
2021-07-06 18:04:34 +0300

    Add DI

--- AzureStorage.MvcWeb/Controllers/TableStoragesController.cs
+++ AzureStorage.MvcWeb/Controllers/TableStoragesController.cs
@@ -10,7 +10,11 @@ namespace AzureStorage.MvcWeb.Controllers
 {
     public class TableStoragesController : Controller
     {
-        private readonly INoSqlStorage<Product> _product;
+        private readonly INoSqlStorage<Product> _noSqlStorage;
+        public TableStoragesController(INoSqlStorage<Product> noSqlStorage)
+        {
+            _noSqlStorage = noSqlStorage;
+        }
         public IActionResult Index()
         {
             return View();
