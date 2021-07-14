42151464bf0d1a1ab0c626f2b0b01f894b6311fd
Gokce Ozel <norm.developer@algomedi.com>
2021-07-14 15:52:30 +0300

    Start To Implement Blob Storage

---
+++ AzureStorage.Library/IBlobStorage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AzureStorage.Library
+{
+    public  interface IBlobStorage
+    {
+        public string BlobUrl { get; set; }
+        Task UploadAsync(Stream fileStream, string name, EContainerName eContainerName);
+        Task<Stream> DowloadAsync(string fileName, EContainerName eContainerName);
+        Task DeleteAsync(string fileName);
+        Task SetLogAsync(string text, string fileName);
+        Task<List<string>> GetLogAsync(string text, string fileName);
+        List<string> GetNames(EContainerName eContainerName);
+    }
+    public enum EContainerName
+    {
+        Picture,
+        Pdf,
+        Logs
+    }
+}
---
+++ AzureStorage.Library/Services/BlobStorage.cs
@@ -0,0 +1,49 @@
+﻿using Azure.Storage.Blobs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureStorage.Library.Services
+{
+    public class BlobStorage : IBlobStorage
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+        public BlobStorage()
+        {
+            _blobServiceClient = new BlobServiceClient(ConnectionStrings.AzureStorageConnectionString);
+        }
+        public string BlobUrl { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Task DeleteAsync(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stream> DowloadAsync(string fileName, EContainerName eContainerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetLogAsync(string text, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetNames(EContainerName eContainerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetLogAsync(string text, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UploadAsync(Stream fileStream, string name, EContainerName eContainerName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
