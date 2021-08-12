1f787f327846423914c522a307aad72c8215d7ab
norm.developer <norm.developer@algomedi.com>
2021-08-12 11:42:40 +0300

    Revert "delete project necessary"
    This reverts commit 40b9f08cf9240392e68f2b4294011a177b2e7093.

---
+++ AzureStorage.MvcWeb/Controllers/TableStoragesController.cs
@@ -0,0 +1,24 @@
+﻿using AzureStorage.Library;
+using AzureStorage.Library.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureStorage.MvcWeb.Controllers
+{
+    public class TableStoragesController : Controller
+    {
+        private readonly INoSqlStorage<Product> _noSqlStorage;
+        public TableStoragesController(INoSqlStorage<Product> noSqlStorage)
+        {
+            _noSqlStorage = noSqlStorage;
+        }
+        public IActionResult Index()
+        {
+            var test = _noSqlStorage.GetAll().ToList();
+            return View();
+        }
+    }
+}
